9c105e30a69f95bbfce548da81f35054672d93a9
Shedelbower <alec.shedelbower@gmail.com>
2019-08-07 06:56:35 -0500

    Added start screen.

---
+++ Minecraft/Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    private string sceneToLoad;
+    private bool isTransitioning = false;
+    private float transitionTimer = 0.0f;
+    private static readonly float TRANSITION_DURATION = 0.5f;
+    
+
+    public void TransitionToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void BeginTransitionToScene(string sceneName)
+    {
+        //Canvas canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+        loadingScreen.SetActive(true);
+        sceneToLoad = sceneName;
+        isTransitioning = true;
+    }
+
+    private void Update()
+    {
+        if (isTransitioning)
+        {
+            transitionTimer += Time.deltaTime;
+            if (transitionTimer >= TRANSITION_DURATION)
+            {
+                TransitionToScene(sceneToLoad);
+            }
+
+        }
+    }
+}
